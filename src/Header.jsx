a7d93c2757bd7109729c02eb6e3e8c39f597b811
samkosal <67755908+samkosal@users.noreply.github.com>
2026-04-18 19:04:38 -0700

    missing logo fixed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,11 @@
 import './Header.css'
-import coffeeCup from './images/brown-coffee-cup.png'
+// import coffeeCup from './images/brown-coffee-cup.png'
 
 export default function Header() {
     return (
         <div className="header-container">
             <header className="header">
-                <img src={coffeeCup} alt="Brown Coffee Cup" className="header-logo" />
+                {/* <img src={coffeeCup} alt="Brown Coffee Cup" className="header-logo" /> */}
                 <h1>LLM-Brew</h1>
                 <button className="btn header-Btn">Models</button>
                 <button className="btn header-Btn">Datasets</button>
